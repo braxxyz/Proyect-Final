2822c23b5f54d7a4a6e676570e26b530a1a6534c
nidia <mirandanidiarivera@gmail.com>
2025-11-05 17:40:45 -0600

    clase pasajero completa

---
+++ Pasajero.java
@@ -0,0 +1,33 @@
+public class Pasajero {
+   
+    private String nombre;
+    private String cedula;
+    private String correo;
+
+ 
+    public Pasajero(String nombre, String cedula, String correo) {
+        this.nombre = nombre;
+        this.cedula = cedula;
+        this.correo = correo;
+    }
+
+  
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+   
+    public void mostrarInfo() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Cédula: " + cedula);
+        System.out.println("Correo: " + correo);
+    }
+}
