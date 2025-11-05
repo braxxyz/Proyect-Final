167c49f474e0cfbc97401734dcb7f1fab5e25b95
nidia <mirandanidiarivera@gmail.com>
2025-11-05 17:39:16 -0600

    clase avion completa

---
+++ Avion.java
@@ -0,0 +1,33 @@
+public class Avion {
+    
+    private String codigoAvion;
+    private int capacidadEjecutiva;
+    private int capacidadEconomica;
+
+   
+    public Avion(String codigoAvion, int capacidadEjecutiva, int capacidadEconomica) {
+        this.codigoAvion = codigoAvion;
+        this.capacidadEjecutiva = capacidadEjecutiva;
+        this.capacidadEconomica = capacidadEconomica;
+    }
+
+ 
+    public String getCodigoAvion() {
+        return codigoAvion;
+    }
+
+    public int getCapacidadEjecutiva() {
+        return capacidadEjecutiva;
+    }
+
+    public int getCapacidadEconomica() {
+        return capacidadEconomica;
+    }
+
+   
+    public void mostrarInfo() {
+        System.out.println("Código del avión: " + codigoAvion);
+        System.out.println("Capacidad Ejecutiva: " + capacidadEjecutiva);
+        System.out.println("Capacidad Económica: " + capacidadEconomica);
+    }
+}
